69c3ff7ec14bc4acf41d8f066a732d5013b6c166
FidoHUN <72193667+FidoHUN@users.noreply.github.com>
2020-11-07 13:27:02 +0100

    Add files via upload
    Added input validation and a notification panel.

--- app.js
+++ app.js
@@ -2,24 +2,36 @@ const button = document.getElementById('button');
 button.addEventListener('click', getJokes);
 
 function getJokes() {
-  const howMany = document.getElementById('input').value;
+  const howMany = Number(document.getElementById('input').value);
   const controller = new Controller();
 
-  const result = controller
-    .get(`http://api.icndb.com/jokes/random/${howMany}`)
-    .then((data) => data)
-    .catch((err) => err);
+  if (isNaN(howMany) || howMany <= 0 || howMany === '') {
+    document.getElementById('error').innerHTML = 'The input data is wrong!';
+    document.getElementById('error').classList.add('error');
+    setTimeout(function () {
+      document.getElementById('error').innerHTML = '';
+    }, 3000);
+  } else {
+    const result = controller
+      .get(`http://api.icndb.com/jokes/random/${howMany}`)
+      .then((data) => data)
+      .catch((err) => err);
 
-  let output = '';
+    let output = '';
 
-  result.then((res) => {
-    if (res.type === 'success') {
-      res.value.forEach((element) => {
-        output += `<li>${element.joke}</li>`;
-      });
-      document.getElementById('jokes').innerHTML = output;
-    } else {
-      document.getElementById('jokes').innerHTML = 'Something went wrong... :(';
-    }
-  });
+    result.then((res) => {
+      if (res.type === 'success') {
+        res.value.forEach((element) => {
+          output += `<li>${element.joke}</li>`;
+        });
+        document.getElementById('jokes').innerHTML = output;
+      } else {
+        document.getElementById('error').innerHTML =
+          'Something went wrong... :(';
+        setTimeout(function () {
+          document.getElementById('error').innerHTML = '';
+        }, 3000);
+      }
+    });
+  }
 }
